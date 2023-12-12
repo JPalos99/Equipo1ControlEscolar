0f28ce7ccbf098bcb337fc1573f822ea6c894e86
digis <digis@ALIEN2>
2023-12-12 15:42:33 -0600

    Proyecto

---
+++ src/main/java/com/Digis01/Equipo1ControlEscolar/Equipo1ControlEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.Digis01.Equipo1ControlEscolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo1ControlEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo1ControlEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Digis01/Equipo1ControlEscolar/Equipo1ControlEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Digis01.Equipo1ControlEscolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Equipo1ControlEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
